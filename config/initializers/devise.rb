d57de534af16c8f3944b7640d05b337c2fa81f99
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2021-12-13 13:56:45 +0700

    Add detail book

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '1cd7ade9c88f307546fb0eca4e51cf1a951e12bd8d937b6780d150f0713ed9462d0d4e617272501a9d37b34f3f300048be6d6fcf410dd8989aa23568915dde5e'
+  # config.secret_key = 'ed0f48f793b9e0d9de3cd82f07513ab396ae0280d9ad3877aaf14804c3d0471fdc8163552580ec9ec27a05f0612ea902e9246432937414b39bf93be6b7769d09'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -126,7 +126,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 12
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '2e6a5ce83df35e8f70a979135cc5665e77b5b8fcecacaf3b35328eabcfe6a609fd7cb8cad8477a741bce62a7d8335b744fb2d02a8d6a7d3530b8ae7df321d34b'
+  # config.pepper = '7e15ccb538139a63022bd8f593d8195bd1287a90b02775f0c616cb65243e37f1c5a66974593cd89e47e0218b2a439e58dca1503e097a7fa84da0238b67921595'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_141741) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 2021_11_29_111400) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -39,8 +36,9 @@ ActiveRecord::Schema.define(version: 2021_12_06_141741) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "number_of_pages"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,8 +47,8 @@ ActiveRecord::Schema.define(version: 2021_12_06_141741) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
